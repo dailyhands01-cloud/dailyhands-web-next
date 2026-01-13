0c228149d33a316e8d0b92ad0102d597f2da366a
Ashish Gupta <115079049+ashishgupta09@users.noreply.github.com>
2026-01-13 15:02:26 +0530

    fixed the contact form shadow issue.

--- src/components/contact/ContactInfo.js
+++ src/components/contact/ContactInfo.js
@@ -13,7 +13,7 @@ const ContactInfo = () => {
                                     <span class='fas fa-map-marker-alt' aria-hidden="true"></span>
                                 </div>
                                 <div className="contact-box__single-text">
-                                    <h2><a href="#">Our Location</a></h2>
+                                    <h2><a>Our Location</a></h2>
                                     <p>Spring Velly Flat No. 103,<br />Near Kaylani Training Center,<br />Keshavnagar, Mundhwa,<br />Pune 411036</p>
                                 </div>
                             </div>
@@ -27,7 +27,7 @@ const ContactInfo = () => {
                                     <span className="icon-email"></span>
                                 </div>
                                 <div className="contact-box__single-text">
-                                    <h2><a href="#">Email Address</a></h2>
+                                    <h2><a>Email Address</a></h2>
                                     <p><a href="mailto:info@dailyhands.in">info@dailyhands.in</a></p>
                                     <p><a href="https://dailyhands.in">www.dailyhands.in</a></p>
                                 </div>
@@ -42,7 +42,7 @@ const ContactInfo = () => {
                                     <span className="fa fa-phone"></span>
                                 </div>
                                 <div className="contact-box__single-text">
-                                    <h2><a href="#">Phone Number</a></h2>
+                                    <h2><a>Phone Number</a></h2>
                                     <p><a href="tel:8805779006">+91 88057 79006</a></p>
                                     <p><a href="tel:7385483447">+91 73854 83447</a></p>
                                 </div>
