0d68003c6ae88a425e241a12e6d3332be90ef66c
Ashish Gupta <115079049+ashishgupta09@users.noreply.github.com>
2026-02-02 15:26:15 +0530

    chnage the oages priopety for the sitemap.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,32 @@ module.exports = {
   changefreq: 'daily',
   priority: 1,
   generateIndexSitemap: false,
+  transform: async (config, path) => {
+    let priority = config.priority;
+    let changefreq = config.changefreq;
+
+    if (path === '/') {
+      priority = 1.0;
+    } else if (path === '/service') {
+      priority = 0.9;
+    } else if (
+      ['/about', '/contact', '/pricing', '/blog'].includes(path) ||
+      path.includes('-cleaning') ||
+      path === '/project'
+    ) {
+      priority = 0.8;
+    } else if (['/team', '/faq', '/gallery', '/gallery-feed', '/client'].includes(path) || path.startsWith('/blog/')) {
+      priority = 0.8;
+    } else {
+      priority = 0.8;
+    }
+
+    return {
+      loc: path,
+      changefreq: changefreq,
+      priority: priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 };
